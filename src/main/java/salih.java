95a75911dece1f02c15e65d7a8579eb7bee8dcbf
Yusuf <ysfzorlu.3465@gmail.com>
2022-11-19 21:15:48 +0300

    first commit

---
+++ src/main/java/salih.java
@@ -0,0 +1,14 @@
+public class salih {
+    int sss;
+
+    public int getSss() {
+        return sss;
+    }
+
+    public void setSss(int sss) {
+        this.sss = sss;
+    }
+
+    public salih() {
+    }
+}
